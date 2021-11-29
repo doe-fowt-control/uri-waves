7de1e66e05e4df19a0ed20a88c39de52e07a8131
Shawn Albertson <shawn.albertson@students.olin.edu>
2021-11-29 18:17:05 -0500

    update comments

--- linear-reconstruction/freq_range.m
+++ linear-reconstruction/freq_range.m
@@ -8,16 +8,20 @@ function [w] = freq_range(eta_obs, fs, c, n)
 % window = length(eta_obs) -> # of windows used in Welch method,
 % maximimized because more windows seems to make the result more specific
 % (for regular waves)
-% noverlap = [] -> default to 50% of window size
+% noverlap = (default) -> 50% of window size
+% nfft = (default) -> max(256 or the closest power of 2 to the window size)
 
+% Calculate PSD
 [pxx, f] = pwelch(eta_obs(:, 1), length(eta_obs), [],[], fs);
 
+% Find where power is greater than some cutoff
 thresh = pxx > max(pxx)*c;
 
-% Finally isolate the minimum and maximum frequencies, create a linear
-% array
+% Isolate the minimum and maximum frequencies within this threshold
 lo = min(f(thresh));
 hi = max(f(thresh));
+
+% Create a linear array of frequencies
 f = linspace(lo, hi, n);
 
 % convert to rad/s
