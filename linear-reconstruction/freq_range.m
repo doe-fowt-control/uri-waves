612c279996975b5cb0d826fedd58206ef4fd5957
Shawn Albertson <shawn.albertson@students.olin.edu>
2021-12-06 22:39:07 -0500

    comment and clean up freq_range.m

--- linear-reconstruction/freq_range.m
+++ linear-reconstruction/freq_range.m
@@ -24,97 +24,37 @@ thresh = pxx > max(pxx)*c;
 f_ = f(thresh);
 pxx_ = pxx(thresh);
 
+% Isolate the minimum and maximum frequencies within this threshold
 lo = min(f_);
 hi = max(f_);
 
+% Cumulative sum of area under PSD curve, normalized to be within 0-1
 Q_ = cumtrapz(f_, pxx_) / trapz(f_, pxx_);
 
+% Spline fit to cumsum (0-1) on horizontal, frequencies on vertical
 pp = spline(Q_, f_);
 
+% Evaluate spline fit at n linearly spaced points to get nonlinear spacing
+% back. Multiply by 2*pi to get rad/s
 w = 2*pi*ppval(pp, linspace(0,1,n));
 
-1+1;
-
-% if method == 0
-% % % uncomment here to get og method back
-%     % Isolate the minimum and maximum frequencies within this threshold
-%     lo = min(f(thresh));
-%     hi = max(f(thresh));
-%
-%     % Create a linear array of frequencies
-%     f = linspace(lo, hi, n);
-%
-%     % convert to rad/s
-%     w = 2*pi*f;
-% end
-%
-% if method == 1
-%     frel = f(thresh);
-%     lo = min(frel);
-%     hi = max(frel);
-%
-%     smallest_change = (hi-lo)./(3*n);
-%     size_norm = pxx(thresh)/max(pxx(thresh)); % large value for frequencies of importance
-%     increment = smallest_change.*(1./size_norm);
-%
-%
-%     j = 1;
-%     fdist = [lo];
-%     for i = 1:length(frel)-1
-%         while fdist(j) < frel(i+1) - (frel(i+1)-frel(i))/2
-%             fdist(j+1) = fdist(j) + increment(i);
-%             j = j+1;
-%         end
-%     end
-%
-%     w = 2*pi*fdist;
-% end
 
 
-
-
-function [w] = freq_range(eta_obs, fs, c, n)
-% fs - sampling frequency
-% eta_obs - raw observations
-% c - cutoff threshold for energy container
-% n - number of frequencies to return
-
-% Evaluate spectral density using Welch method
-% window = length(eta_obs) -> # of windows used in Welch method,
-% maximimized because more windows seems to make the result more specific
-% (for regular waves)
-% noverlap = (default) -> 50% of window size
-% nfft = (default) -> max(256 or the closest power of 2 to the window size)
-
-% Calculate PSD
-[pxx, f] = pwelch(eta_obs(:, 1), length(eta_obs), [],[], fs);
-
-%figure()
-%plot(pxx)
-%ylim([-0.1e-4,4e-4])
-%xlim([0, 300])
-
-% Find where power is greater than some cutoff
-thresh = pxx > max(pxx)*c;
-
-% Isolate the minimum and maximum frequencies within this threshold
-lo = min(f(thresh));
-hi = max(f(thresh));
-
-% Create a linear array of frequencies
-%f = linspace(lo, hi, n);
-
+if method == 11
 % Create a biased array of frequencies
 n = n/2;
     vector1 = nonLinspace(lo, thresh, n, nonLinVec);
     vector2 = nonLinspace(thresh, hi, n, nonLinVec);
 
-vector = cat(1,vector1, nonLinVec ) % concatenation of the 2 vectors
+vector = cat(1,vector1, nonLinVec); % concatenation of the 2 vectors
 
 % convert to rad/s
 w = 2*pi*vector;
+end
 
 end
+
+
 
 % -------------------------------------------------------------------------
 % nonLinspace(mn, mx, num, spacetype) returns a vector of non-linearly
